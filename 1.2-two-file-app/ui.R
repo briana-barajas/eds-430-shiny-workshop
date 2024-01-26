3f0747933fcd6a8322197668ecb83f03b8efe837
bbarajas429 <140910852+bbarajas429@users.noreply.github.com>
2024-01-26 13:54:11 -0800

    Added app2 trout panels

--- 1.2-two-file-app/ui.R
+++ 1.2-two-file-app/ui.R
@@ -1,2 +1,46 @@
 # user interface ----
-ui <- fluidPage()
+ui <- navbarPage(
+  
+  title = "LTER Animal Data Explorer",
+  
+  # (PAGE 1) Intro tabPanel ----
+  tabPanel(title = "About this page",
+           
+           "background info goes here"), #END (PAGE 1) Intro tabPanel
+  
+  # (PAGE 2) Data viz tabPanel -----
+  tabPanel(title = "Explore the Data",
+           
+           #tabsetPanel to contain tabs for data viz ----
+           tabsetPanel(
+             
+             # trout tab panel ----
+             tabPanel(title = "Trout",
+                      
+                      # trout sidebarLayout ----
+                      sidebarLayout(
+                        
+                        # trout sidebarPanel ----
+                        sidebarPanel(
+                          "trout inputs will live here"
+                        ), # END trout sidebarPanel
+                        
+                        # trout mainPanel ----
+                        mainPanel(
+                          "this is where my trout output will live"
+                        ) # END trout mainPanel
+                        
+                      ) # END trout sidebarLayout
+                      
+             ), # END trout tabPanel
+             
+             # penguin tab panel ----
+             tabPanel(title = "Penguin",
+                      
+                      "penguin viz goes here")
+             
+           ) # END tabsetPanel
+           
+  ) # END (PAGE 2) data viz tabPanel
+  
+) # END navbarPage
