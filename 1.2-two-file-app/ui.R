ae4ee0d825f82f765b2d5e422801dbdae9db7f4f
bbarajas429 <140910852+bbarajas429@users.noreply.github.com>
2024-01-26 14:49:42 -0800

    Created app2 trout panel and plot

--- 1.2-two-file-app/ui.R
+++ 1.2-two-file-app/ui.R
@@ -22,12 +22,37 @@ ui <- navbarPage(
                         
                         # trout sidebarPanel ----
                         sidebarPanel(
-                          "trout inputs will live here"
+                          
+                          # channel type pickerinput ----
+                          pickerInput(inputId = "channel_type_input",
+                                      label = "Select channel type(s):",
+                                      choices = unique(clean_trout$channel_type),
+                                      selected = c("pool"),
+                                      
+                                      # add select/deselect all option
+                                      options = pickerOptions(actionsBox = TRUE),
+                                      
+                                      multiple = TRUE), # END trout channel pickerinput
+                          
+                          # section checkboxGroupButtons ----
+                          checkboxGroupButtons(inputId = "section_input",
+                                               label = "Select sampling section(s):",
+                                               choices = c("clear cut forest", "old growth forest"),
+                                               selected = c("clear cut forest", "old growth forest"),
+                                               individual = FALSE, #puts buttons next to each other
+                                               justified = TRUE, #width of buttons fill width of panel
+                                               size = "sm",
+                                               checkIcon = list(yes = icon("check"))
+                          ) # END section checkboxGroupButtons
+                          
                         ), # END trout sidebarPanel
                         
                         # trout mainPanel ----
                         mainPanel(
-                          "this is where my trout output will live"
+                          
+                          # trout scatterplot output ----
+                          plotOutput(outputId = "trout_scatterplot_output")
+                          
                         ) # END trout mainPanel
                         
                       ) # END trout sidebarLayout
@@ -37,7 +62,27 @@ ui <- navbarPage(
              # penguin tab panel ----
              tabPanel(title = "Penguin",
                       
-                      "penguin viz goes here")
+                      #penguin sidebarLayout ----
+                      sidebarLayout(
+                        
+                        # penguin sidebarPanel ----
+                        sidebarPanel(
+                          
+                          "penguin inputs will live here"
+                          
+                        ), # END penguin sidebarPanel
+                        
+                        # penguin mainPanel ----
+                        
+                        mainPanel(
+                          
+                          "this is where my penguin output will live"
+                          
+                        ) # END penguin mainPanel
+                        
+                      ) # END penguin sidebarLayout
+                      
+             ) # END penguin tabPanel
              
            ) # END tabsetPanel
            
